9a13cc6851652199ab98ed27b4da75e3394451e6
Ed dos Prazeres <edimilsonpereira.pro@gmail.com>
2019-10-13 20:01:58 -0300

    Add Icons, new assets and update styles colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,7 +1,8 @@
 export default {
-    primary: "#a00303",
+    primary: "#EC1D24",
     secundary: "",
     black: "#21252d",
+    midlack: "#252A33",
     lightBlack: "#282e39",
     white: "#fff",
     transparent: 'transparent'
