f115052b2256e340bffb0c24abb86644d1652fe6
Ed dos Prazeres <edimilsonpereira.pro@gmail.com>
2019-10-13 16:02:17 -0300

    Create new metrics and colors for application

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,3 +1,8 @@
 export default {
-
+    primary: "#a00303",
+    secundary: "",
+    black: "#21252d",
+    lightBlack: "#282e39",
+    white: "#fff",
+    transparent: 'transparent'
 }
--- src/styles/metrics.js
+++ src/styles/metrics.js
@@ -1,5 +1,17 @@
 import { Platform, Dimensions } from 'react-native'
+import { Header } from 'react-navigation-stack';
+
+const sizeScreen = {
+    heightHeader: `${Dimensions.get('window').height - Header.HEIGHT}px`,
+    height: `${Dimensions.get('window').height}`,
+    width: `${Dimensions.get('window').width}`
+}
+
+const baseSpacing = (number = 1) => {
+    return `${number * 8}px`; 
+}
 
 export default {
-  
+    baseSpacing,
+    sizeScreen
 }
