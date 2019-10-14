cf0dd2ce3a59bb10e87f7af1a3d0d943a2f76c57
Ed dos Prazeres <edimilsonpereira.pro@gmail.com>
2019-10-14 00:51:29 -0300

    Update new actions on redux

--- src/redux/reducers/Marvel.js
+++ src/redux/reducers/Marvel.js
@@ -1,7 +1,10 @@
 import api from '../../config/api'
 
 export const Types = {
-  GET_HEROES: 'Marvel/GET_HEROES'
+  GET_HEROES: 'Marvel/GET_HEROES',
+  SET_FEEDBACKS_SEARCH: 'Marvel/SET_FEEDBACKS_SEARCH',
+  UPDATE_SEARCH_QUERY: 'Marvel/UPDATE_SEARCH_QUERY',
+  CLEAR_SEARCH_QUERY: 'Marvel/CLEAR_SEARCH_QUERY'
 }
 
 export const Creators = {
@@ -11,11 +14,37 @@ export const Creators = {
       type: Types.GET_HEROES,
       payload: response,
     }
-  }
+  },
+  setFeedbacksSearch: (loadingSearch) => {
+    return {
+      type: Types.SET_FEEDBACKS_SEARCH,
+      payload: {
+        loadingSearch
+      },
+    }
+  },
+  updateSearchQuery: (query, offset = 0) => {
+    const response = api.get(`/v1/public/characters?orderBy=name&&limit=100&nameStartsWith=${query}&offset=${offset}`) 
+    return {
+      type: Types.UPDATE_SEARCH_QUERY,
+      payload: response
+    }
+  },
+  clearSearchQuery: () => {
+    return {
+      type: Types.CLEAR_SEARCH_QUERY,
+      payload: [],
+    }
+  },
 }
 
 const INITIAL_STATE = {
-  heroes: []
+  heroes: [],
+  searchHeroes: [],
+  searchHeroesLoaded: false,
+  messageSearch: '',
+  querySearch: false,
+  loadingSearch: false,
 }
 
 export default function Marvel(state = INITIAL_STATE, action) {
@@ -24,6 +53,26 @@ export default function Marvel(state = INITIAL_STATE, action) {
       return {
         ...state,
         heroes: [...state.heroes, ...action.payload.data.data.results]
+      }
+
+    case Types.SET_FEEDBACKS_SEARCH: 
+      return {
+        ...state,
+        loadingSearch: action.payload.loadingSearch
+      }
+
+    case Types.UPDATE_SEARCH_QUERY: 
+      return {
+        ...state,
+        searchHeroes: action.payload.data.data.results,
+        searchHeroesLoaded: true,
+      }
+
+    case Types.CLEAR_SEARCH_QUERY: 
+      return {
+        ...state,
+        searchHeroes: action.payload,
+        searchHeroesLoaded: false,
       }
     default:
       return state
