a3a7218453273bd5e6b93093ba182fba2810b8b8
Ed dos Prazeres <edimilsonpereira.pro@gmail.com>
2019-10-15 01:28:10 -0300

    Create Screen Hero Details

---
+++ src/views/HeroDetailsScreen/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container} from './styles'
+import { HeroDetails } from '../../components'
+
+const HeroDetailsScreen = () => {
+  return (
+    <Container>
+      <HeroDetails />      
+    </Container>
+  );
+}
+
+export default HeroDetailsScreen
--- src/views/index.js
+++ src/views/index.js
@@ -1,9 +1,11 @@
 import HeroesScreen from './HeroesScreen'
 import SearchScreen from './SearchScreen'
+import HeroDetailsScreen from './HeroDetailsScreen'
 // Import views here
 
 export {
   HeroesScreen,
   SearchScreen,
+  HeroDetailsScreen,
   // Insert views here
 }
